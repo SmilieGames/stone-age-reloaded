fb997a64244bc926a2d78677b49ed181a772e7d3
G710 <16511117+G710@users.noreply.github.com>
2018-12-04 13:52:14 +0100

    pushed screen into own folder

--- src/components/StoneAgeRenderer.js
+++ src/components/StoneAgeRenderer.js
@@ -6,14 +6,14 @@ import TextField from '@material-ui/core/TextField';
 
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 
 import HomeIcon from '@material-ui/icons/Home';
 import BuildIcon from '@material-ui/icons/Build';
 import StoreIcon from '@material-ui/icons/Store';
 import AutorenewIcon from '@material-ui/icons/Autorenew'
 import SendIcon from '@material-ui/icons/Send'
+
+import Village from './screens/village'
 
 // Constants for the currently shown view (the indexes are used by the bottom navigation)
 const Views = {
@@ -33,12 +33,21 @@ class StoneAgeRenderer extends Component {
     }
 
     this.onEndTurnButtonClicked = this.onEndTurnButtonClicked.bind(this);
+    this.renderScreen = this.renderScreen.bind(this);
   }
 
 
   onEndTurnButtonClicked(){
     this.props.moves.calculate();
     this.props.events.endTurn();
+  }
+
+  // switch case to choose the selected view and render it.
+  renderScreen(){
+      switch(this.state.currentView){
+          case Views.Village:
+            return <Village {...this.props}/>
+      }
   }
   
   render() {
@@ -54,37 +63,11 @@ class StoneAgeRenderer extends Component {
               variant="outlined"
             />
           </Toolbar>
-
         </AppBar>
 
-        <Grid style={{backgroundColor: 'lightGray'}} container spacing={0}>
-            <Tile/>
-            <Tile>
-                <Paper>Agrar</Paper>
-            </Tile>
-            <Tile/>
-            <Tile>
-                <Paper>Culture</Paper>
-            </Tile>
-
-            <Tile/>
-            <Tile>
-                <Paper >Fruits</Paper>
-            </Tile>
-            <Tile/>
-            <Tile>
-                <Paper >Stone</Paper>
-            </Tile>
-
-            <Tile>
-                <Paper >Village</Paper>
-            </Tile>
-            <Tile>
-                <Paper >Fishing</Paper>
-            </Tile>
-            <Tile/>
-            <Tile/>
-        </Grid>
+        <div style={{height: '60vh'}}>
+            { this.renderScreen() }
+        </div>
         
         <BottomNavigation
           value={this.state.currentView}
@@ -100,14 +83,6 @@ class StoneAgeRenderer extends Component {
       </div>
     );
   }
-}
-
-const Tile = (props) => {
-    return (
-        <Grid item xs={3} style={{height: '20vh'}}>
-            {props.children}
-        </Grid>
-    )
 }
 
 export default StoneAgeRenderer;
---
+++ src/components/screens/village.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const Tile = (props) => {
+    return (
+        <Grid item xs={3} style={{height: '20vh'}}>
+            {props.children}
+        </Grid>
+    )
+}
+
+const Village = (props) => {
+    return(
+        <Grid style={{backgroundColor: 'lightGray'}} container spacing={0}>
+            <Tile/>
+            <Tile>
+                <Paper>Agrar {props.G.agrar.currentCitizens}</Paper>
+            </Tile>
+            <Tile/>
+            <Tile>
+                <Paper>Culture</Paper>
+            </Tile>
+
+            <Tile/>
+            <Tile>
+                <Paper >Fruits</Paper>
+            </Tile>
+            <Tile/>
+            <Tile>
+                <Paper >Stone</Paper>
+            </Tile>
+
+            <Tile>
+                <Paper >Village</Paper>
+            </Tile>
+            <Tile>
+                <Paper >Fishing</Paper>
+            </Tile>
+            <Tile/>
+            <Tile/>
+        </Grid>
+    );
+}
+
+export default Village;
