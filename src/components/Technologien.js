679796f1f7ddb4cd2ca85e098cba0cde9b7960ed
Kompethemd <45541295+Kompethemd@users.noreply.github.com>
2018-12-05 15:27:35 +0100

    grobe Vorlage fuer Technologien

---
+++ src/components/Technologien.js
@@ -0,0 +1,14 @@
+//Hier ist die Bauanleitung für Technologien
+
+class Technologien extends CurrentTechStack{
+    constructor(){
+        let vorgaengertechnologie
+        let nachfolgertechnologie
+        let Beschreibung
+        let Auswirkung
+    }
+
+
+
+
+}
