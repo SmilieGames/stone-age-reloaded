210307a76449cadf9cd332126c29eafbac2d69ea
G710 <16511117+G710@users.noreply.github.com>
2018-12-06 00:34:42 +0100

    moved tech to separate file

--- src/components/screens/TechTree.js
+++ src/components/screens/TechTree.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 
+import Tech from '../Tech';
 
 const Tile = (props) => {
   return (
@@ -17,62 +17,6 @@ const Tile = (props) => {
       
     </Grid>
   )
-}
-
-class Tech extends React.Component {
-
-  constructor(props){
-    super(props);
-
-    this.unlockTechnology = this.unlockTechnology.bind(this);
-    this.isClickable = this.isClickable.bind(this);
-    this.isActive = this.isActive.bind(this);
-    this.requirementsMet = this.requirementsMet.bind(this);
-    this.enoughResearchPoints = this.enoughResearchPoints.bind(this);
-  }
-
-  isClickable(){
-    if(!!this.props.tech){
-      if(!this.isActive() && this.requirementsMet() && this.enoughResearchPoints()){
-        return true;
-      }else{
-        return false;
-      }
-    }
-  }
-
-  isActive(){
-    return this.props.tech.active;
-  }
-
-  requirementsMet(){
-    this.props.tech.requirements.forEach((req) => {
-      if(!this.props.G.technologies[req].active){
-        return false;
-      }
-    })
-    return true;
-  }
-
-  enoughResearchPoints(){
-    if(this.props.G.resources.researchPoints >= this.props.tech.cost){
-      return true;
-    }else{
-      return false;
-    }
-  }
-
-  unlockTechnology(){
-    this.props.unlockFunction(this.props.label)
-  }
-
-  render(){
-    return (  
-      <Button disabled={!this.isClickable()} onClick={this.unlockTechnology} variant="contained" style={{ marginTop: '10%', marginLeft: '20%', marginRight: '20%' }}>
-          {this.props.label}
-      </Button>
-    )    
-  }
 }
 
 const TechTree = (props) => {
