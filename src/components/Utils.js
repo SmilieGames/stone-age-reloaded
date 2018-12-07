dcba03a6ee640a66631c7e4210daf54aeda3c4e2
G710 <16511117+G710@users.noreply.github.com>
2018-12-07 13:47:10 +0100

    fixed food production calculation

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -45,7 +45,38 @@ export function estimatedCitizenGrowth(G){
   // we need to handle the two cases if we have a positive or negative factor, as the factors differ for both cases
   const foodConsumption = currentCitizens * G.factors.foodConsumptionPerCitizen
 
+  const totalFood = foodProduction + currentFood;
+  const remainingFood = totalFood - foodConsumption;
+
+  if(foodProduction - foodConsumption < 0){
+    if(remainingFood < 0){
+      return totalFood - foodConsumption;
+    }else{
+      return 0;
+    }
+  }else{
+    return "+" + (foodProduction - foodConsumption) / 10.0;
+  }
+  
+
+  //if(foodConsumption >= totalFood){
+  if(totalFood - foodConsumption > 0){
+    return totalFood - foodConsumption;
+  }else{
+    return "+" + (foodProduction - foodConsumption) / 10.0;
+  }
+
   // if we dont have enough food for everyone, the population will shrink by this factor
+  if(foodProduction - foodConsumption < 0){
+    if(currentFood > currentCitizens){
+      return 0;
+    }else{
+      return currentFood - currentCitizens;
+    }
+  }else{
+    return "+" + (foodProduction - foodConsumption) / 10.0;
+  }
+
   if(currentFood + foodProduction - foodConsumption <= 0){
     return currentFood + foodProduction - foodConsumption;
   // we dont look at the currentFood for the population growth
