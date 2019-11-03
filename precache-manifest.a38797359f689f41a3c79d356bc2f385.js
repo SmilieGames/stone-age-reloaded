f986b167cf84b55fcf8885b2b11562db93fa820b
G710 <16511117+G710@users.noreply.github.com>
2019-11-03 22:07:26 +0100

    Updates

---
+++ precache-manifest.a38797359f689f41a3c79d356bc2f385.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aabf03557afbd9d3810b48f47a160c1",
+    "url": "/stone-age-reloaded/index.html"
+  },
+  {
+    "revision": "5be5e4d3614bbd43ca35",
+    "url": "/stone-age-reloaded/static/css/main.9bc73a06.chunk.css"
+  },
+  {
+    "revision": "6a4e27cf14f2eba3d8ea",
+    "url": "/stone-age-reloaded/static/js/2.f3aa3e16.chunk.js"
+  },
+  {
+    "revision": "5be5e4d3614bbd43ca35",
+    "url": "/stone-age-reloaded/static/js/main.13fe453a.chunk.js"
+  },
+  {
+    "revision": "5940d197bcb76add9984",
+    "url": "/stone-age-reloaded/static/js/runtime-main.1cf2d88e.js"
+  },
+  {
+    "revision": "1c3c537eaf74abde30cd69d033232039",
+    "url": "/stone-age-reloaded/static/media/background.1c3c537e.png"
+  },
+  {
+    "revision": "7377951fd89a31a57426fc63d08e89a3",
+    "url": "/stone-age-reloaded/static/media/button.7377951f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stone-age-reloaded/precache-manifest.8b1c4b24aad735ee03eaceae3a925551.js"
+  "/stone-age-reloaded/precache-manifest.a38797359f689f41a3c79d356bc2f385.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/stone-age-reloaded/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stone-age-reloaded/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
